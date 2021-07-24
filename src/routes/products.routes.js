546ddcec60a1e10ca4d5eaebc1d4f3756c47c858
Jeferson Gonçalves de Freitas <freitas000jeferson@gmail.com>
2021-07-24 20:30:40 -0300

    correcao ordem dos get#2

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,7 +6,7 @@ const {
 } = require('../validations');
 
 router.get('/', validate(find.list), productController.get);
-router.get('/:id', validate(find.id), productController.offers);
+router.get('/:id', validate(find.id), productController.findbyId);
 router.get('/offers', validate(find.list), productController.offers);
 
 module.exports.products = router;
